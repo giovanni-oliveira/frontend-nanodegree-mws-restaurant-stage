49d4796ca811ef82e2ec5413b2950047c9cf3d0c
Giovanni Pereira dos Santos Oliveira <giovanni.psoliveira@gmail.com>
2019-05-14 07:12:08 -0300

    Add to method 'activate' and urls to cache

--- sw.js
+++ sw.js
@@ -1,17 +1,30 @@
-const cacheName = 'restaurant-v1';
+const cacheName = 'restaurant-v2';
 const urlsToCache = [
     '/',
     '/css/styles.css',
     '/js/main.js',
     '/js/dbhelper.js',
     '/js/restaurant_info.js',
-    '/img/'
+    '/index.html',
+    '/restaurant.html'
 ];
 
 self.addEventListener('install', function(event) {
     event.waitUntil(async function(){
         const cache = await caches.open(cacheName);
         await cache.addAll(urlsToCache);
+    }());
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(async function(){
+        const cache = await caches.keys();
+        const filterCache = name => cacheName !== name;
+        const removeCache = name => caches.delete(name);
+
+        return Promise.all(
+            cache.filter(filterCache).map(removeCache)
+        );
     }());
 });
 
